6f0de64b88aeb38c2f5a30a27696cefb50b42ad1
Aditya Kotte <72753795+a-kotte@users.noreply.github.com>
2024-02-16 00:40:57 -0500

    Prompt user for new grid and build to same size

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,28 +1,61 @@
 const defaultGridSize = 16;
 const gridContainer = document.querySelector('.grid-container');
 
-// Create the grid
-for (let i = 0; i < defaultGridSize; i++) {
-  // make the rows
-  let gridRow = document.createElement('div');
-  gridRow.classList.add('grid-row');
-  gridContainer.appendChild(gridRow);
-  // then the columns
-  for (let j = 0; j < defaultGridSize; j++) {
-    let gridSquare = document.createElement('div');
-    gridSquare.classList.add('grid-square');
-    gridRow.appendChild(gridSquare);
-
-  }
+function removeGrid() {
+  let gridRows = document.querySelectorAll('.grid-row');
+  gridRows.forEach((gridRow) => {
+    gridRow.remove();
+  });
 }
 
-// highlight on mousover
-let gridSquares = document.querySelectorAll('.grid-square');
-gridSquares.forEach((gridSquare) => {
-  gridSquare.addEventListener('mouseenter', (event) => {    
-    gridSquare.setAttribute('style', 'background-color: red');
+function buildGrid(gridSize) {
+  // make the rows
+  for (let i = 0; i < gridSize; i++) {    
+    let gridRow = document.createElement('div');
+    gridRow.classList.add('grid-row');
+    gridContainer.appendChild(gridRow);
+    // then the columns
+    for (let j = 0; j < gridSize; j++) {
+      let gridSquare = document.createElement('div');
+      gridSquare.classList.add('grid-square');
+      gridRow.appendChild(gridSquare);  
+    }
+  }
+  highlightSquares();
+}
 
-  });
-}, false);
+function highlightSquares() {  
+  let gridSquares = document.querySelectorAll('.grid-square');
+  gridSquares.forEach((gridSquare) => {
+    gridSquare.addEventListener('mouseenter', (event) => {    
+      gridSquare.setAttribute('style', 'background-color: red');
+
+    });
+  }, false);
+}
+
+// Build the initial grid
+buildGrid(defaultGridSize);
+highlightSquares()
 
 // prompt user for new grid
+function promptForGrid() {
+  let gridSize= prompt('How many squares per side of new grid?');
+  // ensure a number is input
+  while (gridSize > 100) {
+    gridSize = prompt('Please select a number below 100');
+  }
+  while (typeof(Number(gridSize)) !== 'number') {
+    gridSize = prompt('Invalid entry. Please select a number for the grid size.')
+    // ensure number is less than 100
+    
+  }
+  removeGrid();  
+  buildGrid(Number(gridSize));
+}
+let newGridButton = document.querySelector('.new-grid-button');
+newGridButton.addEventListener('click', promptForGrid);
+
+
+
+
